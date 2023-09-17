32262479e2c714d84ee0e3f97b0365870d40c9d9
Markkwang <zwan633@aucklanduni.ac.nz>
2023-09-17 21:12:51 +1200

    Implemented a key inserter for escaping the room

--- src/main/java/nz/ac/auckland/se206/controllers/Room1Controller.java
+++ src/main/java/nz/ac/auckland/se206/controllers/Room1Controller.java
@@ -1,7 +1,13 @@
 package nz.ac.auckland.se206.controllers;
 
+import javafx.event.Event;
+import javafx.event.EventTarget;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Pane;
 import nz.ac.auckland.se206.App;
 import nz.ac.auckland.se206.SceneManager.roomType;
 
@@ -43,5 +49,51 @@ public class Room1Controller {
   private void room3ButtonClicked() {
     System.out.println("Room 3 button clicked");
     App.setUi(roomType.ROOM3);
+  }
+
+  @FXML
+  public void passwordButtonClicked(Event event) {
+    EventTarget target = event.getTarget();
+    Button button = (Button) target;
+    updateKeyLabel(button.getText());
+  }
+
+  @FXML private Label keyLabel;
+
+  @FXML
+  public void updateKeyLabel(String key) {
+    if (key.equals("x")) {
+      keyLabel.setText("");
+      return;
+    }
+    if (keyLabel.getText().length() >= 6) {
+      return;
+    }
+
+    if (key.equals("Enter")) {
+      checkKey();
+      return;
+    }
+
+    String prev = keyLabel.getText();
+    keyLabel.setText(prev + key);
+  }
+
+  private void checkKey() {}
+
+  @FXML Pane keyInserter;
+
+  @FXML ImageView smallKeyInserter;
+
+  @FXML
+  public void openKeyInserter() {
+    keyInserter.setVisible(true);
+    smallKeyInserter.setVisible(false);
+  }
+
+  @FXML
+  public void closeKeyInserter() {
+    keyInserter.setVisible(false);
+    smallKeyInserter.setVisible(true);
   }
 }
