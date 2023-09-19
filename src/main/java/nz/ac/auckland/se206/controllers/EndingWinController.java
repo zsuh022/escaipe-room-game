a112ff5cebd0d433fe2b33e88a17f736b4433bce
Markkwang <zwan633@aucklanduni.ac.nz>
2023-09-20 01:00:35 +1200

    Implement the code base for ending(win) scene and image

--- src/main/java/nz/ac/auckland/se206/controllers/EndingWinController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/EndingWinController.java
@@ -1,6 +1,7 @@
 package nz.ac.auckland.se206.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
 public class EndingWinController {
 
@@ -8,6 +9,22 @@ public class EndingWinController {
   private void initialize() {
     System.out.println("EndingWinController initialized");
   }
+
+  @FXML private Label keyLabel;
+
+  @FXML
+  private void keyLabelEntered() {}
+
+  @FXML
+  private void keyLabelExited() {}
+
+  @FXML private Label timeLabel;
+
+  @FXML
+  private void riddleLabelEntered() {}
+
+  @FXML
+  private void riddleLabelExited() {}
 
   @FXML
   private void exitButtonClicked() {
