bc1e1d9d01f628416e31629ad35a52047f827e70
Liam Black <lbla309@aucklanduni.ac.nz>
2023-09-19 18:05:13 +1200

    buttons added to room 2

--- src/main/java/nz/ac/auckland/se206/controllers/Room2Controller.java
+++ src/main/java/nz/ac/auckland/se206/controllers/Room2Controller.java
@@ -1,12 +1,17 @@
 package nz.ac.auckland.se206.controllers;
 
+import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Rectangle;
 import nz.ac.auckland.se206.App;
 import nz.ac.auckland.se206.GameState;
 import nz.ac.auckland.se206.SceneManager.roomType;
 
 public class Room2Controller {
+  @FXML private Rectangle room2Box;
+  @FXML private Rectangle room2Lock;
 
   /** Initializes the room view, it is called when the room loads. */
   public void initialize() {
@@ -31,5 +36,15 @@ public class Room2Controller {
 
   private void initializeTimer() {
     timeLabel.textProperty().bind(GameState.timeManager.getSecond().asString());
+  }
+
+  @FXML
+  public void room2BoxClicked(MouseEvent event) throws IOException {
+    System.out.println("box clicked");
+  }
+
+  @FXML
+  public void room2LockClicked(MouseEvent event) throws IOException {
+    System.out.println("lock clicked");
   }
 }
