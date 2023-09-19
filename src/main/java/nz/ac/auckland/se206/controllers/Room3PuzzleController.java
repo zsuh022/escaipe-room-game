cea483768954c0cc9d3e9d90459742258672afb5
zsuh022 <zsuh022@aucklanduni.ac.nz>
2023-09-19 16:47:41 +1200

    feat: add puzzle functionality
    Tiles will only switch if the empty tile is adjacent

--- src/main/java/nz/ac/auckland/se206/controllers/Room3PuzzleController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/Room3PuzzleController.java
@@ -1,7 +1,12 @@
 package nz.ac.auckland.se206.controllers;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
 import nz.ac.auckland.se206.App;
 import nz.ac.auckland.se206.SceneManager.roomType;
 
@@ -15,17 +20,52 @@ public class Room3PuzzleController {
   @FXML private Button btn6;
   @FXML private Button btn7;
   @FXML private Button btn8;
-  @FXML private Button btn9;
+  @FXML private Button btnEmpty;
   @FXML private Button btnExitPuzzle;
+  @FXML private GridPane gridPane;
 
-  @FXML
-  private void onTileClicked() {
-    System.out.println("Tile clicked");
-  }
+  private List<Button> buttons;
+  private Map<Button, int[]> initialButtonPositions = new HashMap<>();
 
   @FXML
   private void onBackButtonClicked() {
     System.out.println("Back button clicked");
     App.setUi(roomType.ROOM3);
+  }
+
+  @FXML
+  public void initialize() {
+    buttons = Arrays.asList(btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btnEmpty);
+
+    for (int i = 0; i < buttons.size(); i++) {
+      Button button = buttons.get(i);
+      int[] position = new int[] {i % 3, i / 3};
+      initialButtonPositions.put(button, position);
+      GridPane.setColumnIndex(button, position[0]);
+      GridPane.setRowIndex(button, position[1]);
+    }
+
+    // hide the empty button
+    btnEmpty.setVisible(false);
+  }
+
+  @FXML
+  public void onTileClicked() {
+    Button clickedButton = (Button) gridPane.getScene().getFocusOwner();
+
+    // prevent user from clicking empty tile
+    if (clickedButton == btnEmpty) return;
+
+    int x = GridPane.getColumnIndex(clickedButton);
+    int y = GridPane.getRowIndex(clickedButton);
+    int emptyX = GridPane.getColumnIndex(btnEmpty);
+    int emptyY = GridPane.getRowIndex(btnEmpty);
+
+    if (Math.abs(emptyX - x) + Math.abs(emptyY - y) == 1) {
+      GridPane.setColumnIndex(clickedButton, emptyX);
+      GridPane.setRowIndex(clickedButton, emptyY);
+      GridPane.setColumnIndex(btnEmpty, x);
+      GridPane.setRowIndex(btnEmpty, y);
+    }
   }
 }
