016637d45ae1e2b458634426a91476582e734f6b
zsuh022 <zsuh022@aucklanduni.ac.nz>
2023-09-20 07:58:14 +1200

    feat: add transition to lose ending

--- src/main/java/nz/ac/auckland/se206/controllers/EndingLoseController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/EndingLoseController.java
@@ -1,13 +1,22 @@
 package nz.ac.auckland.se206.controllers;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.animation.Interpolator;
+import javafx.animation.RotateTransition;
+import javafx.animation.Timeline;
+import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+import javafx.util.Duration;
 
-public class EndingLoseController {
+public class EndingLoseController implements Initializable {
+
+  @FXML private ImageView gameMasterLose;
 
   @FXML
-  private void initialize() {
-    System.out.println("EndingWinController initialized");
-  }
+  private void initialize() {}
 
   @FXML
   private void exitButtonClicked() {
@@ -17,5 +26,28 @@ public class EndingLoseController {
   @FXML
   private void restartButtonClicked() {
     System.out.println("Play again button clicked");
+  }
+
+  @Override
+  public void initialize(URL location, ResourceBundle resources) {
+    gameMasterTransition();
+  }
+
+  private void gameMasterTransition() {
+    // game master translation
+    TranslateTransition translateTransition =
+        new TranslateTransition(Duration.seconds(8), gameMasterLose);
+    translateTransition.setFromX(-200);
+    translateTransition.setToX(1124.0);
+    translateTransition.setInterpolator(Interpolator.LINEAR);
+
+    // game master rotation
+    RotateTransition rotateTransition = new RotateTransition(Duration.seconds(4), gameMasterLose);
+    rotateTransition.setCycleCount(Timeline.INDEFINITE);
+    rotateTransition.setByAngle(360);
+    rotateTransition.setInterpolator(Interpolator.LINEAR);
+
+    translateTransition.play();
+    rotateTransition.play();
   }
 }
