119d33b27ff52e1d728eae6dbbc9bfd7dab76561
Liam Black <lbla309@aucklanduni.ac.nz>
2023-09-21 03:37:38 +1200

    for room 2 puzzle 2 whole keyboard and methods for keys implemented

--- src/main/java/nz/ac/auckland/se206/controllers/Room2Puzzle2Controller.java
+++ src/main/java/nz/ac/auckland/se206/controllers/Room2Puzzle2Controller.java
@@ -5,9 +5,159 @@ import javafx.scene.control.Button;
 
 public class Room2Puzzle2Controller {
   @FXML private Button buttonA;
+  @FXML private Button buttonB;
+  @FXML private Button buttonC;
+  @FXML private Button buttonD;
+  @FXML private Button buttonE;
+  @FXML private Button buttonF;
+  @FXML private Button buttonG;
+  @FXML private Button buttonH;
+  @FXML private Button buttonI;
+  @FXML private Button buttonJ;
+  @FXML private Button buttonK;
+  @FXML private Button buttonL;
+  @FXML private Button buttonM;
+  @FXML private Button buttonN;
+  @FXML private Button buttonO;
+  @FXML private Button buttonP;
+  @FXML private Button buttonQ;
+  @FXML private Button buttonR;
+  @FXML private Button buttonS;
+  @FXML private Button buttonT;
+  @FXML private Button buttonU;
+  @FXML private Button buttonV;
+  @FXML private Button buttonW;
+  @FXML private Button buttonX;
+  @FXML private Button buttonY;
+  @FXML private Button buttonZ;
 
   @FXML
   private void buttonAClicked() {
     System.out.println("A");
+  }
+
+  @FXML
+  private void buttonBClicked() {
+    System.out.println("B");
+  }
+
+  @FXML
+  private void buttonCClicked() {
+    System.out.println("C");
+  }
+
+  @FXML
+  private void buttonDClicked() {
+    System.out.println("D");
+  }
+
+  @FXML
+  private void buttonEClicked() {
+    System.out.println("E");
+  }
+
+  @FXML
+  private void buttonFClicked() {
+    System.out.println("F");
+  }
+
+  @FXML
+  private void buttonGClicked() {
+    System.out.println("G");
+  }
+
+  @FXML
+  private void buttonHClicked() {
+    System.out.println("H");
+  }
+
+  @FXML
+  private void buttonIClicked() {
+    System.out.println("I");
+  }
+
+  @FXML
+  private void buttonJClicked() {
+    System.out.println("J");
+  }
+
+  @FXML
+  private void buttonKClicked() {
+    System.out.println("K");
+  }
+
+  @FXML
+  private void buttonLClicked() {
+    System.out.println("L");
+  }
+
+  @FXML
+  private void buttonMClicked() {
+    System.out.println("M");
+  }
+
+  @FXML
+  private void buttonNClicked() {
+    System.out.println("N");
+  }
+
+  @FXML
+  private void buttonOClicked() {
+    System.out.println("O");
+  }
+
+  @FXML
+  private void buttonPClicked() {
+    System.out.println("P");
+  }
+
+  @FXML
+  private void buttonQClicked() {
+    System.out.println("Q");
+  }
+
+  @FXML
+  private void buttonRClicked() {
+    System.out.println("R");
+  }
+
+  @FXML
+  private void buttonSClicked() {
+    System.out.println("S");
+  }
+
+  @FXML
+  private void buttonTClicked() {
+    System.out.println("T");
+  }
+
+  @FXML
+  private void buttonUClicked() {
+    System.out.println("U");
+  }
+
+  @FXML
+  private void buttonVClicked() {
+    System.out.println("V");
+  }
+
+  @FXML
+  private void buttonWClicked() {
+    System.out.println("W");
+  }
+
+  @FXML
+  private void buttonXClicked() {
+    System.out.println("X");
+  }
+
+  @FXML
+  private void buttonYClicked() {
+    System.out.println("Y");
+  }
+
+  @FXML
+  private void buttonZClicked() {
+    System.out.println("Z");
   }
 }
