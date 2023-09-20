3e0e02020e5540da09186dba1b31a37a94f02aca
Markkwang <zwan633@aucklanduni.ac.nz>
2023-09-20 18:57:21 +1200

    Implment the protopye working for blue and red circles

--- src/main/java/nz/ac/auckland/se206/DraggableManager.java
+++ src/main/java/nz/ac/auckland/se206/DraggableManager.java
@@ -1,13 +1,14 @@
 package nz.ac.auckland.se206;
 
 import javafx.scene.Node;
+import javafx.scene.shape.Circle;
 
 public class DraggableManager {
   private double mouseX;
   private double mouseY;
+  private final double threholdConstant = 0.25;
 
-  public void makeDraggable(Node node) {
-
+  public void makeDraggable(Node node, Circle targetCircle) {
     node.setOnMouseClicked(
         mouseEvent -> {
           mouseX = mouseEvent.getX();
@@ -18,6 +19,20 @@ public class DraggableManager {
         mouseEvent -> {
           node.setLayoutX(mouseEvent.getSceneX() - mouseX);
           node.setLayoutY(mouseEvent.getSceneY() - mouseY);
+          checkAndSnapToTarget(node, targetCircle);
         });
+  }
+
+  private void checkAndSnapToTarget(Node node, Circle targetCircle) {
+    double distance =
+        Math.sqrt(
+            Math.pow(node.getLayoutX() - targetCircle.getLayoutX(), 2)
+                + Math.pow(node.getLayoutY() - targetCircle.getLayoutY(), 2));
+    double threshold = threholdConstant * (targetCircle.getRadius() + ((Circle) node).getRadius());
+
+    if (distance < threshold) {
+      node.setLayoutX(targetCircle.getLayoutX());
+      node.setLayoutY(targetCircle.getLayoutY());
+    }
   }
 }
--- src/main/java/nz/ac/auckland/se206/controllers/Room3Puzzle2Controller.java
+++ src/main/java/nz/ac/auckland/se206/controllers/Room3Puzzle2Controller.java
@@ -1,3 +1,18 @@
 package nz.ac.auckland.se206.controllers;
 
-public class Room3Puzzle2Controller {}
+import javafx.fxml.FXML;
+import javafx.scene.shape.Circle;
+import nz.ac.auckland.se206.DraggableManager;
+
+public class Room3Puzzle2Controller {
+
+  @FXML private Circle redCircle;
+  @FXML private Circle blueCircle;
+
+  DraggableManager draggableManager = new DraggableManager();
+
+  @FXML
+  private void initialize() {
+    draggableManager.makeDraggable(redCircle, blueCircle);
+  }
+}
