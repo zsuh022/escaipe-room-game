a6a1436369a4d4dab46dd68974c618fc449494bd
Markkwang <zwan633@aucklanduni.ac.nz>
2023-09-20 19:56:36 +1200

    Added all the needed images on to scenebuilder

--- src/main/java/nz/ac/auckland/se206/controllers/Room3Puzzle2Controller.java
+++ src/main/java/nz/ac/auckland/se206/controllers/Room3Puzzle2Controller.java
@@ -1,18 +1,17 @@
 package nz.ac.auckland.se206.controllers;
 
 import javafx.fxml.FXML;
-import javafx.scene.shape.Circle;
 import nz.ac.auckland.se206.DraggableManager;
 
 public class Room3Puzzle2Controller {
 
-  @FXML private Circle redCircle;
-  @FXML private Circle blueCircle;
+  // @FXML private Circle redCircle;
+  // @FXML private Circle blueCircle;
 
   DraggableManager draggableManager = new DraggableManager();
 
   @FXML
   private void initialize() {
-    draggableManager.makeDraggable(redCircle, blueCircle);
+    // draggableManager.makeDraggable(redCircle, blueCircle);
   }
 }
