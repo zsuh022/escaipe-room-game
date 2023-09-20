d56e6785b9bf596939e2ba8080d090c434821704
Liam Black <lbla309@aucklanduni.ac.nz>
2023-09-20 16:19:33 +1200

    buttons for memory game added

--- src/main/java/nz/ac/auckland/se206/controllers/Room2PuzzleController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/Room2PuzzleController.java
@@ -4,11 +4,58 @@ import javafx.fxml.FXML;
 import javafx.scene.shape.Rectangle;
 
 public class Room2PuzzleController {
-
-  @FXML private Rectangle blue;
+  @FXML private Rectangle one;
+  @FXML private Rectangle two;
+  @FXML private Rectangle three;
+  @FXML private Rectangle four;
+  @FXML private Rectangle five;
+  @FXML private Rectangle six;
+  @FXML private Rectangle seven;
+  @FXML private Rectangle eight;
+  @FXML private Rectangle nine;
 
   @FXML
-  private void clickBlue() {
-    System.out.println("blue clicked");
+  private void clickOne() {
+    System.out.println("one clicked");
+  }
+
+  @FXML
+  private void clickTwo() {
+    System.out.println("two clicked");
+  }
+
+  @FXML
+  private void clickThree() {
+    System.out.println("three clicked");
+  }
+
+  @FXML
+  private void clickFour() {
+    System.out.println("four clicked");
+  }
+
+  @FXML
+  private void clickFive() {
+    System.out.println("five clicked");
+  }
+
+  @FXML
+  private void clickSix() {
+    System.out.println("six clicked");
+  }
+
+  @FXML
+  private void clickSeven() {
+    System.out.println("seven clicked");
+  }
+
+  @FXML
+  private void clickEight() {
+    System.out.println("eight clicked");
+  }
+
+  @FXML
+  private void clickNine() {
+    System.out.println("nine clicked");
   }
 }
